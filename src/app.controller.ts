d31ffa71154153abb0f51c9c4dd2f8db2b754f06
ranvir jha <tappers_callous0h@icloud.com>
2024-03-05 04:09:01 +0530

    Auth0 authentication for API security.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthorizationGuard } from './authorization/authorization.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(AuthorizationGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorizationModule } from './authorization/authorization.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [AuthorizationModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
