5fe9545601cfd067d8e5a16a8da10baf9a2a241e
ranvir jha <tappers_callous0h@icloud.com>
2024-03-16 03:11:01 +0530

    getArtefactById - references

--- src/ops/firestore/firebase/firebase.service.ts
+++ src/ops/firestore/firebase/firebase.service.ts
@@ -46,16 +46,28 @@ export class FirebaseService {
   }
 
 
+  /**
+   * Retrieves an artefact from the Firestore database by its ID and determines its accessibility for the specified user.
+   * @param id The ID of the artefact to retrieve.
+   * @param _sub The subject or identifier of the user.
+   * @returns A Promise resolving to an object containing the artefact data and its accessibility status.
+   * @throws Error if the artefact is not found or if there's an error accessing the database.
+   */
   async getArtefactById(id: string, _sub: string): Promise<SecRecord> {
     const docRef = this.firebase.firestore.collection('artifacts');
 
+    // Retrieve artefact snapshot from Firestore
     const snapshot = await docRef.doc(id).get();
+
+    // Check if the artefact belongs to the specified user
     const doesBelongToUser = await this.doesArtefactBelongToUser(id, _sub);
 
+    // Throw an error if the artefact does not exist
     if (!snapshot.exists) {
       throw new Error('Artefact Not Found');
     }
 
+    // Return the artefact data along with its accessibility status
     return {
       id: snapshot.id,
       isEditable: doesBelongToUser,
@@ -63,6 +75,7 @@ export class FirebaseService {
       ...snapshot.data(),
     } as SecRecord;
   }
+
 
   /**
   * Adds a new artefact to the Firestore database and updates contributors.
